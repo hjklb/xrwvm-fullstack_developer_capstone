b0df58846a573851b054c54fb606aeb2bb88842e
hjklb <geethanjalikdevaraj@gmail.com>
2024-07-02 12:05:05 -0400

    Adding temporary changes to Github

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,13 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import RegisterPanel from "./components/register/Register"
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
     </Routes>
+
   );
 }
 export default App;
