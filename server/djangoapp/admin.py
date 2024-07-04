ef7f8410240d7b8aaa71da5994fb8bce815281de
hjklb <162555822+hjklb@users.noreply.github.com>
2024-07-04 15:58:37 +0530

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -13,10 +13,12 @@ class CarModelInline(admin.TabularInline):
     model = CarModel
     extra = 1
 
+
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'make', 'year')
     list_filter = ('make', 'year')
     search_fields = ('name', 'make__name')
+
 
 # Register models here
 admin.site.register(CarMake)
